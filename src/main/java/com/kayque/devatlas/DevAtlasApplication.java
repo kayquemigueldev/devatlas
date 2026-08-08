44f212817f7f6e53b8e32d9706175dbdb28253ab
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-08-07 23:51:40 -0300

    chore: initialize DevAtlas project

---
+++ src/main/java/com/kayque/devatlas/DevAtlasApplication.java
@@ -0,0 +1,12 @@
+package com.kayque.devatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAtlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAtlasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kayque/devatlas/DevAtlasApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kayque.devatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
